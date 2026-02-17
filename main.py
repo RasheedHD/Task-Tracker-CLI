947923662d2bd6524f66be587724e9fb3f35c249
RasheedHD <rasheed200612@gmail.com>
2026-02-17 22:53:55 +0300

    view list of tasks implemented

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from json import loads, dumps, dump
 from datetime import datetime
 
 class Task:
-    def __init__(self, id, description, status):
+    def __init__(self, id, description, status="todo"):
         self.id = id
         self.description = description
         self.status = status
@@ -16,6 +16,12 @@ class Task:
     def updateStatus(self, status):
         self.status = status
         self.updatedAt = datetime.now()
+
+    def getDateString(self, type):
+        if type == "c":
+            return str(self.createdAt)[:-10]
+        elif type == "u":
+            return str(self.updatedAt)[:-10] if self.updatedAt != "N/A" else "N/A"
     
 
 def convert_to_json(task):
@@ -23,8 +29,8 @@ def convert_to_json(task):
         "id": task.id,
         "description": task.description,
         "status": task.status,
-        "createdAt": str(task.createdAt),
-        "updatedAt": task.updatedAt
+        "createdAt": task.getDateString("c"),
+        "updatedAt": task.getDateString("u")
     }
     return dumps(dic)
 
@@ -45,5 +51,29 @@ def deleteTask(task_id):
             tasks.remove(task)
             break
 
-tasks = [Task(1,2,3), Task(4,3,5), Task(1,1,3), Task(5,3,9)]
-saveTasks(tasks)
+def listTasks(s):
+    tasksToView = []
+    if s == "all":
+        tasksToView = tasks
+    elif s in ["done", "todo", "in-progress"]:
+        for task in tasks:
+            if task.status == s:
+                tasksToView.append(task)
+    else:
+        pass # TODO: throw error
+
+    print("="*96)
+    print(f"{"ID":<5} {"Description":<30} {"Status":<8} {"Created At":<20} {"Updated At":<20}")
+    for task in tasksToView:
+        print(f"{task.id:<5} {task.description:<30} {task.status:<8} {task.getDateString("c"):<20} {task.getDateString("u"):<20}")
+
+
+tasks = [Task(1,"Walk the cat", "done"), Task(2,"Build a project", "done"), Task(3,"Study at home"), Task(4,"Clean the dishes")]
+
+
+listTasks("todo")
+
+
+
+
+#saveTasks(tasks)
