d90e0e9ccee623e6a2ff667cca952bc99546555b
RasheedHD <rasheed200612@gmail.com>
2026-02-17 19:49:44 +0300

    Basic add/delete/update tasks + save

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from json import loads, dumps
+from json import loads, dumps, load, dump
 
 class Task:
     def __init__(self, id, description, status, createdAt, updatedAt):
@@ -7,6 +7,9 @@ class Task:
         self.status = status
         self.createdAt = createdAt
         self.updatedAt = updatedAt
+
+    def updateStatus(self, newStatus):
+        self.status = newStatus
     
 
 def convert_to_json(task):
@@ -22,3 +25,19 @@ def convert_to_json(task):
 def convert_to_dic(task):
     return loads(task)
 
+def saveTasks(tasks):
+    tasksD = list(map(convert_to_json, tasks))
+    with open("tasks.json", "w") as tasksFile:
+        dump(tasksD, tasksFile, indent=4)
+
+def addTask(task):
+    tasks.append(task)
+
+def deleteTask(task_id):
+    for task in tasks:
+        if task.id == task_id:
+            tasks.remove(task)
+            break
+
+tasks = [Task(1,2,3,4,5), Task(4,3,5,4,1), Task(1,1,3,4,1), Task(3,3,3,4,5)]
+saveTasks(tasks)
